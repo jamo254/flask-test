017be016f859cdc3606f7634b7e8c8e565ed26bb
Fred Denis <fredjamo68@gmail.com>
2023-02-17 15:51:53 +0300

    home page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -11,6 +11,6 @@ def hello_world():
 
 @app.route('/login')
 def login_form():
-   return 'Login'
+   return render_template("index.html")
 
 
