2d48e868d0f22ec8dd05665bcf800837285fc849
Fred Denis <fredjamo68@gmail.com>
2023-02-17 15:59:25 +0300

    index page

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ if __name__ == '__main__':
   app.run(host='0.0.0.0')
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/login')
 def login_form():
    return render_template("index.html")
 
